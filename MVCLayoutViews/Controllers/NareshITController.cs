e2d869c50c184c669cccfd18ab2d3cdc5ba7c0da
Your Name <kumarvipin897946@gmail.com>
2023-12-23 10:47:17 +0530

    Add project files.

---
+++ MVCLayoutViews/Controllers/NareshITController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLayoutViews.Controllers
+{
+    public class NareshITController : Controller
+    {
+        public ViewResult Home()
+        {
+            return View();
+        }
+        public ViewResult Register()
+        {
+            return View();
+        }
+        public ViewResult Login()
+        {
+            return View();
+        }
+        public ViewResult Courses()
+        {
+            return View();
+        }
+        public ViewResult About()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVCLayoutViews/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCLayoutViews.MvcApplication" Language="C#" %>
